85c4a708065f39f0d02648aeadbf99021ed2b6f0
Hritik <hb75828@gmail.com>
2023-08-14 15:16:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {Button} from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Ecomm Project</h1>
+       <Button>bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
